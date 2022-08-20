3fe954f8976dba515f9e6c4e4935bbe0ccc354d3
Abhay Jyot <abhayjyot.b@gmail.com>
2022-08-20 18:11:22 +0530

    Second

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import mysql.connector as connector
 
 class DBhelper:
     def __init__(self):
-        self.con = connector.connect(host="localhost", user="root", passwd="Abhayttrsaj7", database="pythontest")
+        self.con = connector.connect(host="localhost", user="root", passwd="root", database="pythontest")
         query='create table if not exists user(userId int primary key, username varchar(200), phone varchar(12))'
         cur = self.con.cursor()
         cur.execute(query)
@@ -27,7 +27,7 @@ class DBhelper:
             print()
             print()
 
-            
+
 #main function
 helper = DBhelper()
 #helper.insert_user(5, "Abhay jjjjj", "1234567890")
